af34ae3f1df0d5912407e5f39427e6963b2b08d2
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2019-12-19 13:38:35 -0500

    tests

---
+++ tests/sum.js
@@ -0,0 +1,11 @@
+var sum = require('../sum');
+var expect = require('chai').expect;
+
+describe('#sum()', function() {
+  
+  context('with number arguments', function() {
+    it('should return sum of arguments', function() {
+      expect(sum(1, 2, 3, 4, 5)).to.equal(15)
+    }) } 
+    
+    )})
