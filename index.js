b7e1d7e5b42ecc915090c95d320b1de29400e13f
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2019-12-19 13:50:54 -0500

    minor

--- index.js
+++ index.js
@@ -19,3 +19,5 @@ function sum(a,b) {
     return a + b;
 
 }
+
+module.exports = sum();
--- test/index.js
+++ test/index.js
@@ -1,11 +1,28 @@
-var sum = require('../index.js');
+var sum = require('../index');
 var expect = require('chai').expect;
 
-describe('#sum()', function() {
+
+it('should return sum of arguments', function() {
+        expect(sum(1, 2)).to.equal(3)
+  }
   
-  context('with number arguments', function() {
-    it('should return sum of arguments', function() {
-      expect(sum(1, 2)).to.equal(3)
-    }) } 
+
+// describe('#sum()', function() {
+  
+//   context('with number arguments', function() {
+//     it('should return sum of arguments', function() {
+//       expect(sum(1, 2)).to.equal(3)
+//     }) } 
     
-    )})
+//     )})
+
+
+// var assert = require('assert');
+// describe('Basic Mocha String Test', function () {
+//  it('should return number of charachters in a string', function () {
+//         assert.equal("Hello".length, 4);
+//     });
+//  it('should return first charachter of the string', function () {
+//         assert.equal("Hello".charAt(0), 'H');
+//     });
+// });
