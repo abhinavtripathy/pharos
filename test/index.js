7a700429a8ba8487b1c67547fd370206f5cb7a1a
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2019-12-19 13:44:21 -0500

    mocha tests

--- test/index.js
+++ test/index.js
@@ -1,11 +1,11 @@
-var sum = require('../sum');
+var sum = require('../index.js');
 var expect = require('chai').expect;
 
 describe('#sum()', function() {
   
   context('with number arguments', function() {
     it('should return sum of arguments', function() {
-      expect(sum(1, 2, 3, 4, 5)).to.equal(15)
+      expect(sum(1, 2)).to.equal(3)
     }) } 
     
     )})
