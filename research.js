f42a56db1c57f812d1429eb56f0dbf0145394575
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2019-12-27 21:50:24 -0500

    mathJS test

--- research.js
+++ research.js
@@ -1,8 +1,8 @@
 const nlp = require('compromise')
 nlp.extend(require('compromise-numbers'))
-
+let math = require('mathjs')
 let doc = nlp("there are $22 areas to improve and twenty thousand things")
 let nums = doc.values().toNumber()
 nums = nums.out('array')
 
-console.log(nums)
+console.log(math.evaluate('(2 * 3)'))
