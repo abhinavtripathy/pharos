ad918a5640a1c441640c76b900aa93d0e93327e1
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2019-12-28 20:03:17 -0500

    minor tests

--- research.js
+++ research.js
@@ -1,6 +1,8 @@
 const nlp = require('compromise')
 nlp.extend(require('compromise-numbers'))
 let math = require('mathjs')
+
+
 let doc = nlp("there are $22 areas to improve and twenty thousand things")
 let nums = doc.values().toNumber()
 nums = nums.out('array')
