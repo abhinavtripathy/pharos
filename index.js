161f1a8dec036cc6060f17ed9f08f6f9b1692158
Abhinav Tripathy <abhinavtripathy99@gmail.com>
2019-12-31 01:51:02 -0500

    minor

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 
-function mathOperations() {
+function interpreter(query) {
 
-
-
+    let sentenceSplit = query.split(".")
+    console.log(sentenceSplit)
 
 }
 
+test = "Hello world. This is a program and it is pretty good. I like it quite a bit a bit. a ."
+
+interpreter(test)
